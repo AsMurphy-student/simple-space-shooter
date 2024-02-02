0798beef50646d3296ce129d620cc8486c2204b1
AsMurphy-student <abm2954@rit.edu>
2024-02-02 13:27:20 -0500

    Fixed issue with enemies leaving screen as i did not reset velocity

--- shooter/Scripts/enemy.gd
+++ shooter/Scripts/enemy.gd
@@ -1,15 +1,18 @@
 extends RigidBody2D
 
-@export var speed = 100
+@export var speed = 50
 
 var PositionToGo
 var moving = false
 var movingRight = false
 var movingLeft = false
 
+var screen_size
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$MoveTimer.start()
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,22 +22,27 @@ func _process(delta):
 			moving = false
 			movingRight = false
 			$MoveTimer.start()
+			return
 	else: if (movingLeft):
 		if (position.x <= PositionToGo):
 			moving = false
 			movingLeft = false
 			$MoveTimer.start()
-	
+			return
+	var velocity = Vector2.ZERO
 	if (moving):
-		var velocity = Vector2.ZERO
 		if (movingRight):
 			velocity.x += 1
 		else: if (movingLeft):
 			velocity.x -= 1
 		
-		velocity = velocity.normalized() * speed
+	velocity.y += 0.5
 		
-		position.x += velocity.x * delta
+	velocity = velocity.normalized() * speed
+		
+	linear_velocity = velocity
+		
+		#position.x += velocity.x * delta
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
@@ -43,27 +51,29 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 
 
 func _on_move_timer_timeout():
-	if (randi_range(1, 100) < 50):
-		var newPos = randi_range(10, 50)
-		if (position.x - newPos <= 0):
-			movingRight = true
-			movingLeft = false
-			moving = true
-			PositionToGo = position.x + newPos
-		else:
-			movingRight = false
-			movingLeft = true
-			moving = true
-			PositionToGo = position.x - newPos
+	$MoveTimer.stop()
+	# if (randi_range(1, 100) < 50):
+	var newPos = randi_range(40, 200)
+	# if (position.x - newPos <= 50):
+	if (position.x < newPos):
+		movingRight = true
+		movingLeft = false
+		moving = true
+		PositionToGo = newPos
 	else:
-		var newPos = randi_range(10, 50)
-		if (position.x + newPos >= 320):
-			movingRight = false
-			movingLeft = true
-			moving = true
-			PositionToGo = position.x - newPos
-		else:
-			movingRight = true
-			movingLeft = false
-			moving = true
-			PositionToGo = position.x + newPos
+		movingRight = false
+		movingLeft = true
+		moving = true
+		PositionToGo = newPos
+	#else:
+		#var newPos = randi_range(10, 50)
+		#if (position.x + newPos >= 200):
+			#movingRight = false
+			#movingLeft = true
+			#moving = true
+			#PositionToGo = position.x - newPos
+		#else:
+			#movingRight = true
+			#movingLeft = false
+			#moving = true
+			#PositionToGo = position.x + newPos
